40d132032884b02b3d59ed0f051f536fd3130918
Vladislav Gritsenko <xiaomomi3node@gmail.com>
2023-09-10 03:54:15 +0300

    update config

--- sections/index.ts
+++ sections/index.ts
@@ -1,10 +1,10 @@
-import Hero from './Hero.tsx'
-import About from './About'
-import Explore from './Explore'
-import GetStarted from './GetStarted'
-import WhatsNew from './WhatsNew'
-import World from './World'
-import Insights from './Insights'
-import Feedback from './Feedback'
+import Hero from './Hero.tsx';
+import About from './About.tsx';
+import Explore from './Explore.tsx';
+import GetStarted from './GetStarted.tsx';
+import WhatsNew from './WhatsNew.tsx';
+import World from './World.tsx';
+import Insights from './Insights.tsx';
+import Feedback from './Feedback.tsx';
 
 export { Hero, About, Explore, GetStarted, WhatsNew, World, Insights, Feedback }
