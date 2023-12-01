e48d8ae3ca3a9c4f7c0df21eb42d9786a8e61548
Vladislav Gritsenko <xiaomomi3node@gmail.com>
2023-12-01 18:02:51 +0200

    update project for subdomain
    remove module-federation

--- next.config.js
+++ next.config.js
@@ -1,31 +1,9 @@
-const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, options) => {
-    const { isServer } = options;
     config.experiments = { topLevelAwait: true };
 
-    // config.module.rules.push({
-    //   test: /\.css$/,
-    //   use: ["style-loader", "css-loader", "postcss-loader"],
-    // });
-
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "metaversus",
-        filename: "static/chunks/remoteEntry.js",
-        remotes: {
-          portfolio: `portfolio@http://localhost:3000/_next/static/${
-            isServer ? "ssr" : "chunks"
-          }/remoteEntry.js`,
-        },
-        exposes: {
-          "./sharedPage": "./pages/index.tsx",
-        },
-      })
-    );
     return config;
   },
 };
